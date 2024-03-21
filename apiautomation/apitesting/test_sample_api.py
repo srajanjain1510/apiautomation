0ea36dc49e74343bab6247aed963669404d49646
srajan jain <srajan.jain@macys.com>
2024-03-21 21:03:53 +0530

    commited 1st test method

--- apiautomation/apitesting/test_sample_api.py
+++ apiautomation/apitesting/test_sample_api.py
@@ -5,5 +5,11 @@ def test_a():
 
     response = requests.get('https://jsonplaceholder.typicode.com/users/1/albums')
 
+    response_json = response.json()
+    user_id = response_json["userId"]
+    user_id_set = set(user_id)
+
+    assert user_id_set == 1
+
     assert response.status_code == 200
 
